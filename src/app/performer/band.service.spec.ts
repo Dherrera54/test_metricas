a28d2e340d880503946e8fe655c7a919c9f8e62c
Dario Herrera <d.herrerag@uniandes.edu.co>
2021-04-24 02:10:15 -0500

    FeatureHU03: Prueba unitaraia crear 10 bandas(verde)

--- src/app/performer/band.service.spec.ts
+++ src/app/performer/band.service.spec.ts
@@ -1,17 +1,47 @@
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, async, inject,getTestBed } from '@angular/core/testing';
 import { BandService } from './band.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import faker from 'faker';
+import {Band }from './performer';
+import { environment } from "../../environments/environment";
+import { HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 
 describe('Service: Band', () => {
+  let injector: TestBed;
+  let service: BandService;
+  let httpMock: HttpTestingController;
+  let apiUrl = environment.baseUrl + "bands";
+
  beforeEach(() => {
    TestBed.configureTestingModule({
      imports: [HttpClientTestingModule],
      providers: [BandService]
    });
+   injector = getTestBed();
+   service = injector.get(BandService);
+   httpMock = injector.get(HttpTestingController);
  });
+ afterEach(() => {
+  httpMock.verify({ ignoreCancelled: true });
+});
 
- it('should create service', inject([BandService], (service: BandService) => {
-   expect(service).toBeTruthy();
- }));
+ it('Method getBands() should return 10 records', () => {
+  let mockPosts: Band[] = [];
+  for (let i = 0; i < 10; i++) {
+    let band = new Band(faker.lorem.sentence(),
+                        faker.lorem.sentence(),
+                        faker.lorem.sentence(),
+                        faker.random.number());
+    mockPosts.push(band);
+  }
+
+  service.getBands().subscribe((bands) => {
+    expect(bands.length).toBe(10);
+  });
+
+  const req = httpMock.expectOne(apiUrl);
+  expect(req.request.method).toBe('GET');
+  req.flush(mockPosts);
+});
+
 });
