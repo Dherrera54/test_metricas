ffd2e2d222a48c6243259dedef24c7b19c4a2b58
Dario Herrera <d.herrerag@uniandes.edu.co>
2021-04-24 01:22:27 -0500

    FeatureHU03: Success 7 0f 8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 
-const routes: any = [
-  { path: '', component: HomeComponent },
-];
+const routes: any = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
