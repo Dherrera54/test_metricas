46850e44d7294095b37bcbeba9ac4ad41f2e4751
Dario Herrera <d.herrerag@uniandes.edu.co>
2021-04-24 01:12:33 -0500

    FeatureHU03: prueba unitaria test

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,25 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home.component';
 
+
 describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+ let component: HomeComponent;
+ let fixture: ComponentFixture<HomeComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
-  }));
+ beforeEach(async(() => {
+   TestBed.configureTestingModule({
+     imports: [HttpClientModule],
+     declarations: [HomeComponent]
+   })
+     .compileComponents();
+ }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+ beforeEach(() => {
+   fixture = TestBed.createComponent(HomeComponent);
+   component = fixture.componentInstance;
+   fixture.detectChanges();
+ });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+ it('should create', () => {
+   expect(component).toBeTruthy();
+ });
 });
