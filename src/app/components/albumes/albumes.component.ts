76155fe769f697509381c9d1f618d9fc15220b6a
Alejito29 <w.gonzalezg@uniandes.edu.co>
2021-04-26 11:20:55 -0500

    FeatureHU001: Creacion del componente albumes para la vista

--- src/app/components/albumes/albumes.component.ts
+++ src/app/components/albumes/albumes.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-albumes',
   templateUrl: './albumes.component.html',
-  styleUrls: ['./albumes.component.css']
+  styleUrls: ['./albumes.component.scss']
 })
 export class AlbumesComponent implements OnInit {
 
