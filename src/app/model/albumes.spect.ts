308a090953469a59ab582a204920e8d9239f6949
Alejito29 <w.gonzalezg@uniandes.edu.co>
2021-04-26 17:00:51 -0500

    Feature HU01: Integracion de prueba unitaria para modelo

--- src/app/model/albumes.spect.ts
+++ src/app/model/albumes.spect.ts
@@ -1,7 +1,18 @@
 import {Albumes} from './albumes';
+import faker from 'faker';
 
 describe('Creacion del objecto Albumes', () => {
   it('should create an instance', () => {
     expect(new Albumes()).toBeTruthy();
+  });
+});
+
+describe('Creacion del objecto Albumes with faker', () => {
+  it('should create an instance', () => {
+    const image = faker.image.imageUrl();
+    const albumes = new Albumes(faker.lorem.sentence(), faker.lorem.sentence(), faker.lorem.sentence(),
+      faker.lorem.sentence(), faker.lorem.sentence()
+      , faker.lorem.sentence(), image);
+    expect(albumes.cover).toEqual(image);
   });
 });
