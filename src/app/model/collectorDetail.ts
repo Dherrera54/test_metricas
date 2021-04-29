28468fd216b81a8c981eaf9f3e924b5863ae6b37
Angelica <am.rodriguez12@uniandes.edu.co>
2021-04-29 12:54:05 -0500

    FeatureHU06: Se corrige la visualizacion de albumes

--- src/app/model/collectorDetail.ts
+++ src/app/model/collectorDetail.ts
@@ -2,10 +2,7 @@ import { Albumes } from "./albumes";
 import { Collector } from "./collector";
 import { Performer } from "./performer";
 
-
-
-
 export class CollectorDetail extends Collector{
-  albumes:Array<Albumes>;
-  fPerformers:Array<Performer>;
+  collectorAlbums:Array<Albumes>;
+  favoritePerformers:Array<Performer>;
 }
