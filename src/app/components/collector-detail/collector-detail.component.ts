0f8601b75c770a4fc1f935ab644d7e76ec031d40
Angelica <am.rodriguez12@uniandes.edu.co>
2021-04-30 18:50:33 -0500

    FeatureHU06: Se corrige obtencion de albumes de collecionista

--- src/app/components/collector-detail/collector-detail.component.ts
+++ src/app/components/collector-detail/collector-detail.component.ts
@@ -4,6 +4,8 @@ import {EventEmitter, Input, Output} from '@angular/core';
 import { CollectorDetail } from 'src/app/model/collectorDetail';
 import { ActivatedRoute } from '@angular/router';
 import { CollectorService } from 'src/app/services/collector.service';
+import { Albumes } from 'src/app/model/albumes';
+import { AlbumesService } from 'src/app/services/albumes..service';
 
 @Component({
   selector: 'app-collector-detail',
@@ -14,18 +16,31 @@ export class CollectorDetailComponent implements OnInit {
 
   collectorDetail: CollectorDetail;
   id:number;
+  albumsCollector: Array<Albumes>;
 
-  constructor(private activatedRoute: ActivatedRoute, private collectorService: CollectorService) { }
+
+  constructor(private activatedRoute: ActivatedRoute, private collectorService: CollectorService, private albumesService:AlbumesService) { }
 
   ngOnInit() {
     this.id = this.activatedRoute.snapshot.params.id;
     this.getDetailCollector();
+    this.getCollectorAlbums();
   }
 
 
   getDetailCollector(): void {
     const item: Array<CollectorDetail> =  this.collectorService.getCollectorsDatos();
     this.collectorDetail = item.find(t => t.id == this.id);
+  }
+
+  getCollectorAlbums():void{
+
+    const items: Array<Albumes> = this.collectorDetail.collectorAlbums;
+    const listaAlbumes: Array<Albumes> = this.albumesService.getAlbumes();
+
+    this.albumsCollector =listaAlbumes.filter(a => items.find(b => a.id === b.id));
+
+
   }
 
 }
