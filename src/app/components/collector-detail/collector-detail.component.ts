09fcbb2331dc931058938397470730934b1b744c
Angelica <am.rodriguez12@uniandes.edu.co>
2021-04-30 23:44:47 -0500

    FeatureHU06: Se agrega vista de cover de albumes

--- src/app/components/collector-detail/collector-detail.component.ts
+++ src/app/components/collector-detail/collector-detail.component.ts
@@ -6,17 +6,21 @@ import { ActivatedRoute } from '@angular/router';
 import { CollectorService } from 'src/app/services/collector.service';
 import { Albumes } from 'src/app/model/albumes';
 import { AlbumesService } from 'src/app/services/albumes..service';
+import { CommentDetail } from 'src/app/model/commentDetail';
+import { TitlesTables } from 'src/app/shared/models/titlesTables';
 
 @Component({
   selector: 'app-collector-detail',
   templateUrl: './collector-detail.component.html',
-  styleUrls: ['./collector-detail.component.css']
+  styleUrls: ['./collector-detail.component.scss']
 })
 export class CollectorDetailComponent implements OnInit {
 
   collectorDetail: CollectorDetail;
   id:number;
   albumsCollector: Array<Albumes>;
+  albumesOfComments: Array<Albumes>;
+  titlesTable: Array<TitlesTables>;
 
 
   constructor(private activatedRoute: ActivatedRoute, private collectorService: CollectorService, private albumesService:AlbumesService) { }
@@ -25,6 +29,7 @@ export class CollectorDetailComponent implements OnInit {
     this.id = this.activatedRoute.snapshot.params.id;
     this.getDetailCollector();
     this.getCollectorAlbums();
+    //this.getCommentsAlbum();
   }
 
 
@@ -42,6 +47,27 @@ export class CollectorDetailComponent implements OnInit {
 
 
   }
+
+  getCommentsAlbum():void{
+
+    const comments: Array<CommentDetail> = this.collectorDetail.comments;
+    const todosAlbumes: Array<Albumes> = this.albumesService.getAlbumes();
+    let p:number =0;
+
+    for (let e of comments){
+      for(let i of todosAlbumes){
+        for(let c of i.comments){
+          if(c.id == e.id){
+            this.albumesOfComments[p]=i;
+            console.log(this.albumesOfComments[p].name);
+            p++;
+          }
+        }
+      }
+    }
+
+  }
+
 
   goBack(): void {
     window.history.back();
--- src/app/model/collectorDetail.ts
+++ src/app/model/collectorDetail.ts
@@ -1,8 +1,10 @@
 import { Albumes } from "./albumes";
 import { Collector } from "./collector";
+import { CommentDetail } from "./commentDetail";
 import { Performer } from "./performer";
 
 export class CollectorDetail extends Collector{
   collectorAlbums:Array<Albumes>;
   favoritePerformers:Array<Performer>;
+  comments:Array<CommentDetail>
 }
